1804ad49e686d8711ff734fcccc3017009249bc9
Zeldais <kamixzero@gmail.com>
2024-10-31 15:07:33 +0700

    Update PlanDBContext.java

--- Asssignment/src/java/dal/PlanDBContext.java
+++ Asssignment/src/java/dal/PlanDBContext.java
@@ -1,6 +1,11 @@
 
 package dal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.*;
 
 /**
@@ -11,7 +16,69 @@ public class PlanDBContext extends DBContext<Plan>{
 
     @Override
     public void insert(Plan model) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            connection.setAutoCommit(false);
+            String sql_insert_plan = "INSERT INTO [Plan]\n"
+                    + "           ([startTime]\n"
+                    + "           ,[endTime]\n"
+                    + "           ,[did])\n"
+                    + "     VALUES\n"
+                    + "           (?\n"
+                    + "           ,?\n"
+                    + "           ,?)";
+            String sql_select_plan = "SELECT @@IDENTITY as plid";
+            String sql_insert_campain = "INSERT INTO [PlanCampaign]\n"
+                    + "           ([plid]\n"
+                    + "           ,[pid]\n"
+                    + "           ,[quantity]\n"
+                    + "           ,[unitEffort])\n"
+                    + "     VALUES\n"
+                    + "           (?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?)";
+            
+            PreparedStatement stm_insert_plan = connection.prepareStatement(sql_insert_plan);
+            stm_insert_plan.setDate(1, model.getStartTime());
+            stm_insert_plan.setDate(2, model.getEndTime());
+            stm_insert_plan.setInt(3, model.getD().getId());
+            stm_insert_plan.executeUpdate();
+            
+            PreparedStatement stm_select_plan = connection.prepareStatement(sql_select_plan);
+            ResultSet rs = stm_select_plan.executeQuery();
+            if(rs.next())
+            {
+                model.setId(rs.getInt("plid"));
+            }
+            for (PlanCampain campain : model.getCampains()) {
+                PreparedStatement stm_insert_campain = connection.prepareStatement(sql_insert_campain);
+                stm_insert_campain.setInt(1, model.getId());
+                stm_insert_campain.setInt(2, campain.getP().getId());
+                stm_insert_campain.setInt(3, campain.getQuantity());
+                stm_insert_campain.setFloat(4, campain.getEffort());
+                stm_insert_campain.executeUpdate();
+            }
+            connection.commit();
+        } catch (SQLException ex) {
+            Logger.getLogger(PlanDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            try {
+                connection.rollback();
+            } catch (SQLException ex1) {
+                Logger.getLogger(PlanDBContext.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ex) {
+                Logger.getLogger(PlanDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(PlanDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
     }
 
     @Override
