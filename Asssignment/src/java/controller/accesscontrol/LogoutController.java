4a4720d51b03f4d144b9157f098c7d2485b3525f
Zeldais <kamixzero@gmail.com>
2024-10-31 16:20:46 +0700

    acesss

--- Asssignment/src/java/controller/accesscontrol/LogoutController.java
+++ Asssignment/src/java/controller/accesscontrol/LogoutController.java
@@ -1,21 +1,32 @@
 
 package controller.accesscontrol;
 
-import dal.UserDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.accesscontrol.User;
 
 /**
  *
  * @author Zeldais
  */
-public class LoginController extends HttpServlet {
+public class LogoutController extends HttpServlet {
    
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       request.getSession().setAttribute("account", null);
+       response.getWriter().println("logged out!");
+    } 
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
      * Handles the HTTP <code>GET</code> method.
@@ -27,10 +38,7 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        
-        //
-        request.getRequestDispatcher("login.html").forward(request, response);
-        //
+        processRequest(request, response);
     } 
 
     /** 
@@ -43,20 +51,7 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        String user = request.getParameter("username");
-        String pass = request.getParameter("password");
-        UserDBContext db = new UserDBContext();
-        User account = db.get(user, pass);
-        
-        if(account!=null)
-        {
-            request.getSession().setAttribute("account", account);
-            response.getWriter().println("login successful!");
-        }
-        else
-            response.getWriter().println("login failed!");
-        
-        response.getWriter().println(getInitParameter("private"));
+        processRequest(request, response);
     }
 
     /** 
