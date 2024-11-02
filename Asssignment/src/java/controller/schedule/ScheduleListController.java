d3446064d1450c73087ab7d9b75586a0e75ca41b
Zeldais <kamixzero@gmail.com>
2024-11-02 14:14:59 +0700

    Update ScheduleListController.java

--- Asssignment/src/java/controller/schedule/ScheduleListController.java
+++ Asssignment/src/java/controller/schedule/ScheduleListController.java
@@ -1,11 +1,13 @@
-
 package controller.schedule;
 
 import controller.accesscontrol.BaseRBACController;
+import dal.ScheduleDBContext;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import model.Schedule;
 import model.accesscontrol.User;
 
 /**
@@ -13,11 +15,13 @@ import model.accesscontrol.User;
  * @author Zeldais
  */
 public class ScheduleListController extends BaseRBACController {
-   
-    
+
     @Override
     protected void doAuthorizedGet(HttpServletRequest request, HttpServletResponse response, User loggeduser) throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ScheduleDBContext scheduleDb = new ScheduleDBContext();
+        ArrayList<Schedule> schedules = scheduleDb.list();
+        request.setAttribute("schedules", schedules);
+        request.getRequestDispatcher("/view/schedule/list.jsp").forward(request, response);
     }
 
     @Override
